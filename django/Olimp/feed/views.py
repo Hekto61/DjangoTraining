ff4006806e4222aaaafe29d296d73c03cd0cb3f1
Hekto61 <94539922+Hekto61@users.noreply.github.com>
2022-10-03 18:27:51 +0300

    Add files via upload

--- django/Olimp/feed/views.py
+++ django/Olimp/feed/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from .models import Olimp
 # Create your views here.
 
+
+def print_Matrix(A): 
+    print()
+    for row in A:
+        for x in row:
+            print('{:2d}'.format(x), end='')
+        print()
+
+
+
 def olimp_lists(request):
     olimps = Olimp.objects.all()
     s=[]
     for olimp in olimps:
         if 'inf' in olimp.tags:
             s.append(olimp)
-    return render(request,'feed/olimp_lists.html' ,{'s': s})
+            
+    
+    bic = {'table': [[ j*i for j in range(1,10)] for i in range(1,10) ] , "name": "Hekto", "role": "Admin"}
+    
+    return render(request,'feed/olimp_lists.html' ,bic)#{'s': s})
 
