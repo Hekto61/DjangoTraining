3f48f17b7cd87795c59383dd4d5001ced0921dae
Hekto61 <94539922+Hekto61@users.noreply.github.com>
2022-10-04 18:20:00 +0300

    Add files via upload

--- django/Olimp/feed/views.py
+++ django/Olimp/feed/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Olimp
+import random
 # Create your views here.
 
 
@@ -20,7 +21,20 @@ def olimp_lists(request):
             s.append(olimp)
             
     
-    bic = {'table': [[ j*i for j in range(1,10)] for i in range(1,10) ] , "name": "Hekto", "role": "Admin"}
+    bic = {'table': [[ j*i for j in range(1,10)] for i in range(1,10) ] , 
+           "name": "Hekto", "role": "Admin", 
+           'bruh':[[ 'bruh ' for j in range(1,10)] for i in range(1,10) ],
+           'randommass': [[ random.randint(0,11) for j in range(1,10)] for i in range(1,10)],
+           'colors':['#FFC0CB',
+                     '#4B0082',
+                     '#FF0000',
+                     '#FF4500',
+                     '#F0E68C',
+                     '#3CB371',
+                     '#40E0D0',
+                     '#87CEEB',
+                     '#8B4513',
+                     '#2F4F4F'][random.randint(0,9)]}
     
     return render(request,'feed/olimp_lists.html' ,bic)#{'s': s})
 
